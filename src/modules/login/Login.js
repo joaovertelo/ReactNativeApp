1ea222c4afc056406887ce173b096a41e2b1ef8b
João Vertelo <joaovertelo@gmail.com>
2018-11-08 23:16:42 -0200

    adicionando componentes

--- src/modules/login/Login.js
+++ src/modules/login/Login.js
@@ -1,14 +1,88 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+
+import { StyleSheet, View, Image, StatusBar, TouchableHighlight, ScrollView } from 'react-native';
+import { Actions } from 'react-native-router-flux';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { Text, Icon } from 'react-native-elements';
+import { RkTextInput, RkButton } from 'react-native-ui-kitten';
+
+import Spinner from 'react-native-loading-spinner-overlay';
+
+// import { changeValue, autenticarUsuario } from './loginActions';
 
 class Login extends Component {
+	constructor(props) {
+		super(props);
+		this._autenticarUsuario = this._autenticarUsuario.bind(this);
+		this.state = {};
+	}
+
+	componentDidMount() {
+		//this._autenticarUsuario();
+	}
+
+	_autenticarUsuario() {
+		let { email, senha } = this.props.login;
+		email = 'joaovertelo@gmail.com';
+		senha = '123456';
+		this.props.autenticarUsuario({ email, senha });
+	}
+
 	render() {
 		return (
 			<View>
-				<Text> Olá mundo</Text>
+				<Text>ola</Text>
 			</View>
 		);
 	}
 }
 
 export default Login;
+
+const styles = StyleSheet.create({
+	scrollView: {
+		flex: 1
+	},
+	container: {
+		flex: 1,
+		backgroundColor: '#FFFFFF',
+		padding: 20,
+		justifyContent: 'center',
+		alignContent: 'center'
+	},
+	logo: {
+		flex: 3,
+		justifyContent: 'center',
+		alignItems: 'center',
+		paddingBottom: 10
+	},
+	form: {
+		flex: 5
+	},
+	bottom: {
+		flex: 1,
+		justifyContent: 'center',
+		flexDirection: 'row'
+	},
+	viewBotoes: {
+		flexDirection: 'row'
+	},
+	viewBotoesSociais: {
+		flexDirection: 'row',
+		justifyContent: 'center',
+		alignItems: 'center',
+		padding: 20
+	},
+	botoes: {
+		flex: 1,
+		height: 60
+	},
+	botoesSocial: {
+		width: 70
+	},
+	msgErro: {
+		color: 'red',
+		paddingBottom: 10
+	}
+});
---
+++ src/modules/login/loginActions.js
@@ -0,0 +1,83 @@
+import firebase from 'firebase';
+import b64 from 'base-64';
+import { Actions } from 'react-native-router-flux';
+
+const BASE_ACTION = 'LOGIN/';
+
+export const CHANGE_VALUE = BASE_ACTION + 'CHANGE';
+export const CADASTRA_USUARIO_SUCESSO = BASE_ACTION + 'CADASTRA_USUARIO_SUCESSO';
+export const CADASTRA_USUARIO_ERRO = BASE_ACTION + 'CADASTRA_USUARIO_ERRO';
+export const AUTENTICAR_USUARIO = BASE_ACTION + 'AUTENTICAR_USUARIO';
+export const LOGIN_SUCESSO = BASE_ACTION + 'LOGIN_SUCESSO';
+export const LOGIN_ERROR = BASE_ACTION + 'LOGIN_ERROR';
+
+export function changeValue(name, value) {
+	return {
+		type: CHANGE_VALUE,
+		name: name,
+		payload: value
+	};
+}
+
+export function cadastraUsuario(login) {
+	return (dispatch) => {
+		firebase
+			.auth()
+			.createUserWithEmailAndPassword(login.email, login.senha)
+			.then((user) => {
+				let emailCrypt = b64.encode(login.email);
+				alert(emailCrypt);
+				firebase
+					.database()
+					.ref('/usuario/' + emailCrypt)
+					.push({ nome: login.nome })
+					.then((value) => cadastroUsuarioSucesso(login, dispatch));
+			})
+			.catch((error) => cadastroUsuarioErro(error, dispatch));
+	};
+}
+
+function cadastroUsuarioSucesso(login, dispatch) {
+	dispatch({
+		type: CADASTRA_USUARIO_SUCESSO,
+		payload: login
+	});
+	Actions.login();
+	alert(login.nome + ' cadastrado com sucesso');
+}
+
+function cadastroUsuarioErro(error, dispatch) {
+	dispatch({
+		type: CADASTRA_USUARIO_ERRO
+	});
+	Actions.inicio();
+}
+
+export const autenticarUsuario = ({ email, senha }) => {
+	return (dispatch) => {
+		dispatch(setLoading(true));
+
+		firebase
+			.auth()
+			.signInWithEmailAndPassword(email, senha)
+			.then((value) => loginSucesso(dispatch))
+			.catch((error) => loginErro(error, dispatch));
+
+		dispatch(setLoading(false));
+	};
+};
+
+const loginSucesso = (dispatch) => {
+	dispatch({
+		type: LOGIN_SUCESSO
+	});
+	Actions.inicio();
+};
+
+const loginErro = (error, dispatch) => {
+	dispatch({
+		type: LOGIN_ERROR,
+		payload: error.message
+	});
+	Actions.inicio();
+};
